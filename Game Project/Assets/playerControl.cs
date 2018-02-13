c25ab5b9d2060214a856a846d7d273c4e80b3e6c
nfrosa <nicollefrosa@gmail.com>
2018-02-12 20:46:39 -0500

    sprites now move and turn around

--- Game Project/Assets/playerControl.cs
+++ Game Project/Assets/playerControl.cs
@@ -8,10 +8,12 @@ public class playerControl : MonoBehaviour {
 
 	private Rigidbody2D rbody;
 	private Vector2 input;
+    private Animator animator;
 
-	void Start (){
+    void Start (){
 		rbody = gameObject.GetComponent<Rigidbody2D>();
-	}
+        animator = this.GetComponent<Animator>();
+    }
 		
 	void FixedUpdate () {
 		input.x = Input.GetAxisRaw("Horizontal") * maxSpeed;
@@ -19,4 +21,27 @@ public class playerControl : MonoBehaviour {
 
 		rbody.MovePosition((Vector2)transform.position + input);
 	}
+    // src: http://michaelcummings.net/mathoms/creating-2d-animated-sprites-using-unity-4.3
+    void Update()
+    {
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+
+        if (vertical > 0)
+        {
+            animator.SetInteger("Direction", 2);
+        }
+        else if (vertical < 0)
+        {
+            animator.SetInteger("Direction", 0);
+        }
+        else if (horizontal > 0)
+        {
+            animator.SetInteger("Direction", 1);
+        }
+        else if (horizontal < 0)
+        {
+            animator.SetInteger("Direction", 3);
+        }
+    }
 }
