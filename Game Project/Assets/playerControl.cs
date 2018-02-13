a7497954ae6c29f1316d4fe47c9036c3e38aab44
Cody LaFlamme <claflamme123@gmail.com>
2018-02-12 19:35:34 -0500

    added new scene

---
+++ Game Project/Assets/playerControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour {
+
+	public float maxSpeed;
+
+	private Rigidbody2D rbody;
+	private Vector2 input;
+
+	void Start (){
+		rbody = gameObject.GetComponent<Rigidbody2D>();
+	}
+		
+	void FixedUpdate () {
+		input.x = Input.GetAxisRaw("Horizontal") * maxSpeed;
+		input.y = Input.GetAxisRaw("Vertical") * maxSpeed;
+
+		rbody.MovePosition((Vector2)transform.position + input);
+	}
+}
