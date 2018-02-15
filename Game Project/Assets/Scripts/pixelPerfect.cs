cef1b81432459baddcfd17862aa9e111f77e4322
Cody LaFlamme <claflamme123@gmail.com>
2018-02-14 20:21:24 -0500

    Fixes screen tearing in tilemap
    - created a "pixel snap" material for sprites that aligns everything to a pixel grid
    - added a script that scales the camera so that art is rendered "pixel perfect"

---
+++ Game Project/Assets/Scripts/pixelPerfect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pixelPerfect : MonoBehaviour {
+
+	private Camera cam;
+
+	private int magnification = 2;
+	private int PPU = 16;
+
+	//source: https://blogs.unity3d.com/2015/06/19/pixel-perfect-2d/
+	void Start () {
+		cam = gameObject.GetComponent<Camera>();
+		cam.orthographicSize = 0.5f*Display.main.renderingHeight/(magnification*PPU);
+	}
+}
