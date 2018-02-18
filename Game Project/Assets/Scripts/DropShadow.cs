e14428c0d990edbbb75e27728d4e0efee436bfd4
nfrosa <nicollefrosa@gmail.com>
2018-02-18 15:55:28 -0500

    attempted to work on shadows, sorted materials

---
+++ Game Project/Assets/Scripts/DropShadow.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropShadow : MonoBehaviour {
+
+    public Vector2 offset = new Vector2(-3, -3);
+
+    private SpriteRenderer sCaster;
+    private SpriteRenderer sShadow;
+
+    private Transform tCaster;
+    private Transform tShadow;
+
+    public Material shadowMaterial;
+    public Color shadowColor;
+
+
+    // Use this for initialization
+    void Start () {
+        tCaster = transform;
+        tShadow = new GameObject().transform;
+        tShadow.parent = tCaster;
+        tShadow.gameObject.name = "shadow";
+        //tShadow.localRotation = Quaternion.identity;
+
+        sCaster = GetComponent<SpriteRenderer>();
+        sShadow = tShadow.gameObject.AddComponent<SpriteRenderer>();
+
+        sShadow.material = shadowMaterial;
+        sShadow.color = shadowColor;
+        //sShadow.sortingLayerName = sCaster.sortingLayerName;
+        //sShadow.sortingOrder = sCaster.sortingOrder-1;
+
+        }
+
+
+    void LateUpdate()
+    {
+        tShadow.position = new Vector2(tCaster.position.x + offset.x,
+            tCaster.position.y + offset.y);
+        sShadow.sprite = sCaster.sprite;
+    }
+
+
+    //source: https://www.youtube.com/watch?v=ft4HUL2bFSQ
+}
