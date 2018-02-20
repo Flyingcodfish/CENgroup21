1a2fa7820aaf6ba95d016e25ec14b53067702a13
Julian <frardvark@gmail.com>
2018-02-20 16:41:25 -0500

    Health Bar
    Added health bar and some other base HUD stuff

---
+++ Game Project/Assets/Scripts/HealthSlider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSlider : MonoBehaviour
+{
+
+    private int maxValue;
+    private int value;
+    public Player player;
+
+    private Slider slider;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        slider = GetComponent<Slider>();
+        maxValue = player.maxHealth;
+        slider.maxValue = maxValue;
+        value = player.getHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        value = player.getHealth();
+        slider.value = value;
+    }
+}
