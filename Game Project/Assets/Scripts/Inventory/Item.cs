f84c2602ce5e8341db284e1d6211bc3dfca72e72
Tyler G Johnson <satchel15555@gmail.com>
2018-02-25 22:12:26 -0500

    added small inventory and minimap
    has an inventory which stacks one item and a extra camera which we can use layers and different scale for a minimap

---
+++ Game Project/Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType { MANA,HEALTH}; // creates types for specific in game items 
+
+public class Item : MonoBehaviour {
+    public ItemType type;
+
+    public Sprite spriteNeutral, spriteHighlighted;
+
+    public int maxSize;
+
+	public void Use()
+    {
+        switch (type)
+        {
+            case ItemType.HEALTH:
+                break;
+            case ItemType.MANA:
+                break;
+        }
+    }
+}
