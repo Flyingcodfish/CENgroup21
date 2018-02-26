1b6197d8dc2017efa0133fc063ad38f79520833d
Cody LaFlamme <claflamme123@gmail.com>
2018-02-26 00:20:33 -0500

    Fixed slime slow-effect bug
    Fixed an issue where killing a slime while slowed would leave the player slowed forever.
    
    In this process, Actor.Die() was made into a coroutine. PlayerControl was updated as a result of this change.
    
    Also, ACTUALLY DOUBLE FOR REAL fixed ObjectTiles. 95% confidence.

--- Game Project/Assets/Scripts/ObjectTile.cs
+++ Game Project/Assets/Scripts/ObjectTile.cs
@@ -5,34 +5,32 @@ using UnityEngine.Tilemaps;
 //Useful for when placing things that should sort, collide, or otherwise behave as more than just tiles: trees and doors, for instance.
 [CreateAssetMenu(fileName = "New Object Tile", menuName = "Object Tile")]
 public class ObjectTile : TileBase {
-	
-	//representative sprite
-	public Sprite preview;
 
 	//gameObject to spawn
 	public GameObject spawnedPrefab;
 	public Vector3 offset = new Vector3(0.5f, 0.5f, 0); //(0.5, 0.5, 0) moves object to center of tile
 
-	//add fields to imitate a normal tile
-	public Color color = Color.white;
-	private Matrix4x4 transform = Matrix4x4.identity;
+	//tile related fields
 	private TileFlags flags = TileFlags.InstantiateGameObjectRuntimeOnly;
-	public Tile.ColliderType colliderType = Tile.ColliderType.Sprite; //TODO mess with this; collision might be from gameObject spawned
+	public Tile.ColliderType colliderType = Tile.ColliderType.Sprite;
 
 	//return sprite information when asked
 	public override void GetTileData(Vector3Int location, ITilemap tileMap, ref TileData tileData){
+
 		tileData.sprite = spawnedPrefab.GetComponent<SpriteRenderer>().sprite;
 		tileData.gameObject = spawnedPrefab;
 
-		tileData.color = this.color;
-		tileData.transform = this.transform;
 		tileData.flags = this.flags;
 		tileData.colliderType = this.colliderType;
 	}
 
+	//sets gameObject position. HideFlags stuff prevents unity from being dumb
 	override public bool StartUp(Vector3Int location, ITilemap tilemap, GameObject instance){
+		this.hideFlags = HideFlags.None;
+
 		if (instance != null){
 			instance.transform.position += offset;
+			instance.hideFlags = HideFlags.DontSaveInEditor;
 		}
 		return true;
 	}
