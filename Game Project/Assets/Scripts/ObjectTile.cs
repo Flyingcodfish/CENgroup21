6f23d3a024b6db601ed9045f752368fa0446b142
Cody LaFlamme <claflamme123@gmail.com>
2018-02-25 22:17:04 -0500

    *actually* fixed ObjectTiles
    (probably) fixed ObjectTiles.
    This fix attempts to prevent an in-editor bug that is difficult to reproduce. Thus, it might still be broken, but probably isn't. :)

--- Game Project/Assets/Scripts/ObjectTile.cs
+++ Game Project/Assets/Scripts/ObjectTile.cs
@@ -5,9 +5,9 @@ using UnityEngine.Tilemaps;
 //Useful for when placing things that should sort, collide, or otherwise behave as more than just tiles: trees and doors, for instance.
 [CreateAssetMenu(fileName = "New Object Tile", menuName = "Object Tile")]
 public class ObjectTile : TileBase {
-
+	
 	//representative sprite
-	public Sprite sprite;
+	public Sprite preview;
 
 	//gameObject to spawn
 	public GameObject spawnedPrefab;
@@ -16,13 +16,12 @@ public class ObjectTile : TileBase {
 	//add fields to imitate a normal tile
 	public Color color = Color.white;
 	private Matrix4x4 transform = Matrix4x4.identity;
-	private TileFlags flags = TileFlags.LockColor;
+	private TileFlags flags = TileFlags.InstantiateGameObjectRuntimeOnly;
 	public Tile.ColliderType colliderType = Tile.ColliderType.Sprite; //TODO mess with this; collision might be from gameObject spawned
 
 	//return sprite information when asked
 	public override void GetTileData(Vector3Int location, ITilemap tileMap, ref TileData tileData){
-		tileData.sprite = this.sprite;
-		
+		tileData.sprite = spawnedPrefab.GetComponent<SpriteRenderer>().sprite;
 		tileData.gameObject = spawnedPrefab;
 
 		tileData.color = this.color;
@@ -31,10 +30,10 @@ public class ObjectTile : TileBase {
 		tileData.colliderType = this.colliderType;
 	}
 
-	override public bool  StartUp(Vector3Int location, ITilemap tilemap, GameObject gObject){
-		if (gObject != null)
-			gObject.transform.position = gObject.transform.position + offset;
-
-		return base.StartUp(location, tilemap, gObject);
+	override public bool StartUp(Vector3Int location, ITilemap tilemap, GameObject instance){
+		if (instance != null){
+			instance.transform.position += offset;
+		}
+		return true;
 	}
 }
