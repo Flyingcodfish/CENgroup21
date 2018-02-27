aabdf33fa2a65ec46bb95453e2ed2898be77ad22
Tyler <satchel15555@gmail.com>
2018-02-26 21:18:27 -0500

    Add files via upload
    added scripts through upload

---
+++ Game Project/Assets/Scripts/inventory/HandleCanvas.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandleCanvas : MonoBehaviour {// used to scale the Hud for different screen sizes 
+
+    private CanvasScaler scaler;
+
+	// Use this for initialization
+	void Start () {
+        scaler = GetComponent<CanvasScaler>();
+
+        scaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+	}
+	
+}
