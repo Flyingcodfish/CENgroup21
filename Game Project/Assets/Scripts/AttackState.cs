dffedec25f1404e3c1e1275b8e69ab0747a89165
Cody LaFlamme <claflamme123@gmail.com>
2018-03-13 17:36:52 -0400

    Fixed pause/inventory function
    Also cleaned up a couple scripts used to set the active state of hitboxes, and made a generic one: AttackState. See slime and skeleton animator controllers for use.

--- Game Project/Assets/Scripts/AttackState.cs
+++ Game Project/Assets/Scripts/AttackState.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlimeAttackState: StateMachineBehaviour {
+public class AttackState : StateMachineBehaviour {
 
 	//normalized time points in attack animation.
 	//hitbox should be active in these bounds; upper bound is exclusive
-	//active during frames 5,6,7 of 10 frames
-	float hitStart = 0.5f;
-	float hitEnd = 0.8f;
+	public float hitStart;
+	public float hitEnd;
 
 	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerindex){
 		if (stateInfo.normalizedTime >= hitStart){
@@ -22,4 +21,5 @@ public class SlimeAttackState: StateMachineBehaviour {
 	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerindex){
 		animator.SetBool("Attacking", false);
 	}
+
 }
