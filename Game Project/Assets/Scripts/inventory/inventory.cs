06b0004291d97b7862ae671e9a995ce53a14fd4f
Tyler G Johnson <satchel15555@gmail.com>
2018-03-17 14:07:33 -0400

    Added deleting items and icon following mouse
    added a way to delete items by moving from inventory onto non game object
    and added icon that follows around mouse

--- Game Project/Assets/Scripts/inventory/inventory.cs
+++ Game Project/Assets/Scripts/inventory/inventory.cs
@@ -1,9 +1,11 @@
 ﻿﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class inventory : MonoBehaviour {
+    // used for inventory layout 
 	private RectTransform inventoryRect;
 
 	private float inventoryWidth, inventoryHeight;
@@ -12,15 +14,27 @@ public class inventory : MonoBehaviour {
 
 	public float slotPaddingLeft, slotPaddingTop, slotSize;
 
+    // used for moving stuff around and maintaining inventory 
+
 	public GameObject slotPrefab;
 
-	private slot from, to;
+    public GameObject iconPrefab;
+
+    private static GameObject hoverObject;
+
+    public Canvas canvas;
+
+    private float hoverYOffset;
+
+	private static slot from, to;
+
+    public EventSystem eventSystem;
 
 	private List<GameObject> allSlots;
 
 	private static int emptySlots;
 
-	public static int EmptySlots
+	public static int EmptySlots // used to get emptySlots in other scripts 
 	{
 		get
 		{
@@ -40,12 +54,31 @@ public class inventory : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-	void Update () {
-
+	void Update () { // doesnt follow mouse
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (!eventSystem.IsPointerOverGameObject(-1) && from != null) // if mouse pointer not over game object 
+            {
+                from.GetComponent<Image>().color = Color.white;
+                from.ClearSlot();
+                Destroy(GameObject.Find("Hover"));
+                to = null;
+                from = null;
+                hoverObject = null;
+            }
+        }
+        if(hoverObject != null)
+        {
+            float xm = Input.mousePosition.x; // finds mouse position 
+            float ym = Input.mousePosition.y;
+            hoverObject.transform.position = new Vector2(xm + 1, ym + 1); // makes object follow mouse 
+        }
 	}
-	private void CreateLayout()
+	private void CreateLayout() // creates the inventory layout based on fields and formulas 
 	{
 		allSlots = new List<GameObject>();
+
+        hoverYOffset = slotSize * 0.01f;
 
 		emptySlots = slots;
 
@@ -111,7 +144,7 @@ public class inventory : MonoBehaviour {
 		}
 		return false;
 	}
-	private bool PlaceEmpty(Item item)
+	private bool PlaceEmpty(Item item) // finds next empty slot to place items in returns false if no slot 
 	{
 		if (emptySlots > 0)
 		{
@@ -128,21 +161,35 @@ public class inventory : MonoBehaviour {
 		}
 		return false;
 	}
-	public void MoveItem(GameObject clicked)
+	public void MoveItem(GameObject clicked) // first gets from object from first click then to object and moves the item stacks if possible 
 	{
 		if (from == null)
 		{
 			if (!clicked.GetComponent<slot>().IsEmpty)
 			{
 				from = clicked.GetComponent<slot>();
-				from.GetComponent<Image>().color = Color.gray;
-			}
+				from.GetComponent<Image>().color = Color.gray; // shows selected 
+
+                hoverObject = (GameObject)Instantiate(iconPrefab);
+                hoverObject.GetComponent<Image>().sprite = clicked.GetComponent<Image>().sprite;
+                hoverObject.name = "Hover";
+
+                RectTransform hoverTransform = hoverObject.GetComponent<RectTransform>();
+                RectTransform clickedTransform = clicked.GetComponent<RectTransform>();
+
+                hoverTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, clickedTransform.sizeDelta.x);
+                hoverTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, clickedTransform.sizeDelta.y);
+
+                hoverObject.transform.SetParent(GameObject.Find("HUDCanvas").transform, true);
+                hoverObject.transform.localScale = from.gameObject.transform.localScale;
+            }
 		}
 		else if(to == null)
 		{
 			to = clicked.GetComponent<slot>();
+            Destroy(GameObject.Find("Hover"));
 		}
-		if(to != null && from != null)
+		if(to != null && from != null) // switches the item stacks arround if one slot empty clears from and adds it to to 
 		{
 			Stack<Item> tmpTo = new Stack<Item>(to.Items);
 			to.Additems(from.Items);
@@ -156,9 +203,10 @@ public class inventory : MonoBehaviour {
 				from.Additems(tmpTo);
 			}
 
-			from.GetComponent<Image>().color = Color.white;
-			to = null;
+			from.GetComponent<Image>().color = Color.white; // resets color 
+			to = null; // sets to null to allow for other objects to be moved 
 			from = null;
+            hoverObject = null;
 		}
 	}
 }
