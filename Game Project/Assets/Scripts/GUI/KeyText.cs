4adf4f19075ddcb0431a3e063e57cfbd10910055
Cody LaFlamme <claflamme123@gmail.com>
2018-03-26 17:58:01 -0400

    Adds a mana bar and key counter to the player HUD.

---
+++ Game Project/Assets/Scripts/GUI/KeyText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyText : MonoBehaviour {
+
+	PlayerControl player;
+	Text text;
+	int count;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag("Player").GetComponent<PlayerControl>();
+		text = GetComponent<Text>();
+		count = player.hasKeys;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//slightly more expensive when we need to change count; much less expensive all other times
+		if (player.hasKeys != count){
+			count = player.hasKeys;
+			text.text = count.ToString();
+		}
+		
+	}
+}
