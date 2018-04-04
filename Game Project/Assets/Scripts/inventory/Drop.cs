4c6e0cd1752f7d70d3db9e07bffcd1cc7f71fd43
Cody LaFlamme <claflamme123@gmail.com>
2018-04-04 17:19:54 -0400

    Enemies drop health and mana on death.
    Created a flexible system that allows customizable enemy drops.
    Existing enemies have been given default drop chances:
    60% nothing, 20% health, 20% mana.
    For new enemies to drop things, they should be given "DeathDropper"
    components, or children objects with that component, as described in the
    DeathDropper.cs file.

---
+++ Game Project/Assets/Scripts/inventory/Drop.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DropType{
+	NONE, KEY, HEALTH, MANA, MONEY_S, MONEY_M, MONEY_L
+}
+
+[RequireComponent(typeof(Collider2D))]
+public class Drop : MonoBehaviour {
+
+	public DropType dropType;
+	static int healAmount = 10;
+	static float manaAmount = 10f;
+
+	void OnTriggerEnter2D(Collider2D other){
+		PlayerControl player = other.gameObject.GetComponentInParent<PlayerControl>(); //allows player attack hitboxes to grab drops
+		if (player != null){
+			switch (this.dropType) {
+			case DropType.KEY:
+				player.hasKeys += 1;
+				break;
+			case DropType.HEALTH:
+				player.TakeDamage (-healAmount);
+				break;
+			case DropType.MANA:
+				player.SpendMana (-manaAmount);
+				break;
+				//TODO add cases for money drops; add money fields and HUD elements to playerControl
+			}
+			Destroy (this.gameObject);
+		}
+	}
+
+}
