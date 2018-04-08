5382fb908953636b5412de53ee042afb537af769
Ricardo <Ricardio234@gmail.com>
2018-04-07 23:39:40 -0400

    added trigger that enables enemy when within range. Ai is disabled at start as a result

---
+++ Game Project/Assets/Scripts/Attacks/AreaOfView.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class AreaOfView : MonoBehaviour {
+
+
+	private Actor parentActor;
+	private AI_Actor enemyScript;
+	private Team parentTeam;
+	// Use this for initialization
+	void Start () {
+		this.parentActor = this.GetComponentInParent<Actor>();
+		this.parentTeam = this.GetComponentInParent<TeamComponent>().team;
+		enemyScript = parentActor.GetComponent<AI_Actor>();
+
+	}
+
+	public void OnTriggerEnter2D(Collider2D other) {
+		TeamComponent otherTeam = other.gameObject.GetComponent<TeamComponent>();
+		if (otherTeam.team != this.parentTeam){
+			enemyScript.enabledAI = true;
+
+		}
+
+
+	}
+
+	public void OnTriggerExit2D(Collider2D other) {
+		TeamComponent otherTeam = other.gameObject.GetComponent<TeamComponent>();
+		if (otherTeam.team != this.parentTeam){
+			enemyScript.enabledAI = false;
+			enemyScript.moveVector = Vector3.zero;
+		}
+
+	}
+}
