b9d0649e01c6337a1f389b766d5f180bb7d15509
Julian <frardvark@gmail.com>
2018-04-10 20:04:47 -0400

    Fire Dungeon!
    Mostly Done!
    Todo: Enemies, inventory drops, boss battle

--- Game Project/Assets/Scripts/Attacks/FireBomb.cs
+++ Game Project/Assets/Scripts/Attacks/FireBomb.cs
@@ -22,6 +22,14 @@ public class FireBomb : MonoBehaviour {
         if (other.tag == "Torch")
         {
             other.GetComponent<SpriteRenderer>().sprite = littorch;
+            GameObject lightObject = new GameObject("Torch light");
+            lightObject.transform.position = new Vector3(other.transform.position.x, other.transform.position.y, -1);
+            Light light = lightObject.AddComponent<Light>();
+            light.intensity = 10;
+            light.range = 40;
+            light.renderMode = LightRenderMode.ForcePixel;
+            other.tag = "LitTorch";
+            other.GetComponent<SpriteRenderer>().color = UnityEngine.Color.white;
         }
 		else if (otherTeam == null || otherTeam.team != this.teamComponent.team)
         {
