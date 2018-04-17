80eead85c4c7f461cc192c3a13d4dfea66d4e7e1
Cody LaFlamme <claflamme123@gmail.com>
2018-04-17 12:18:19 -0400

    Revamps teleport pads in dungeons to add effects.

---
+++ Game Project/Assets/Scripts/Scene Loading/GenericTeleporter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GenericTeleporter : MonoBehaviour
+{
+
+	public GameObject teleportAnim;
+	public GameObject teleportSound;
+	public string newSceneName;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Player"))
+		{
+			teleportSound.SetActive(true);
+			teleportAnim.SetActive(true);
+			StartCoroutine(LoadAfterAnim());
+		}
+	}
+
+	public IEnumerator LoadAfterAnim()
+	{
+		yield return new WaitForSeconds(0.6f);
+		float fadeTime = GetComponent<Fading>().BeginFade(1);
+		yield return new WaitForSeconds(fadeTime);
+		SceneManager.LoadScene(newSceneName);
+	}
+}
+
+
